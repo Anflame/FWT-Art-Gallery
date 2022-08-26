ba461cbe52e3150a6be7831cf24515588fee805a
Anflame <anflame@mail.ru>
2022-08-26 09:54:18 +0400

    fixed name of props className, changes import

--- src/Card/Card.tsx
+++ src/Card/Card.tsx
@@ -1,9 +1,9 @@
 import { FC } from "react";
 import styles from './styles.module.scss';
 import images from '../images/cardImg.jpg';
-import { CardProps } from "../comon-types";
+import type { CardProps } from "../comon-types";
 
-export const Card: FC<CardProps> = ({ title, name, img, classname, handleCLick, ...args }) => {
+export const Card: FC<CardProps> = ({ title, name, img, className, handleCLick, ...args }) => {
     return (
         <figure className={styles.card} onClick={handleCLick}>
             <img src={images} alt={title} className={styles.cardImg} />
