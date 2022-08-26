91e23faf98ffd16688c61f687c1505aa967a0ee4
Anflame <anflame@mail.ru>
2022-08-26 09:58:02 +0400

    fixed remarks

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -5,20 +5,20 @@ import cn from 'classnames';
 interface BaseButtonProps {
     handleClick?: () => void;
     className: string;
-    text: ReactNode | string;
+    children: ReactNode;
     isOutlined?: boolean;
     isFilled?: boolean;
     isDisabled?: boolean;
     others?: HTMLButtonElement;
 }
 
-export const Button: FC<BaseButtonProps> = ({ handleClick, className, text, isOutlined, isFilled, ...others }) => {
+export const Button: FC<BaseButtonProps> = ({ handleClick, className, children, isOutlined, isFilled, ...others }) => {
   return (
     <button onClick={handleClick}
       disabled={others.isDisabled} 
       className={cn('logInBtn', className, isOutlined && 'btn_outlined', isFilled && 'btn_filled', others.isDisabled && 'btn_disabled')}
         >
-        {text}
+        {children}
     </button>
   );
 }
