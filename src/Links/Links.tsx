229eae82a52d9b94a2f4e6d99b458c95b025b8c2
Anflame <anflame@mail.ru>
2022-08-26 10:12:17 +0400

    fixed remarks

--- src/Links/Links.tsx
+++ src/Links/Links.tsx
@@ -2,11 +2,11 @@ import { FC, ReactNode } from "react";
 import styles from './styles.module.scss';
 
 interface LinksProps {
-    href: string;
-    children: ReactNode;
-    others?: HTMLAnchorElement;
+  href: string;
+  children: ReactNode;
+  others?: HTMLAnchorElement;
 }
 
-export const Links: FC<LinksProps> = ({href,children,...others}) => {
-    return <a className={styles.link} href={href}>{children}</a>;
+export const Links: FC<LinksProps> = ({ href,children,...others }) => {
+  return <a className={ styles.link } href={ href }>{ children }</a>;
 }
