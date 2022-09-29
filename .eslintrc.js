c2f39f64c17a49ee1cea351a45dbc2af7b81f727
Anflame <anflame@mail.ru>
2022-09-29 12:48:13 +0400

    fixed ci build problem

--- .eslintrc.js
+++ .eslintrc.js
@@ -52,7 +52,7 @@ module.exports = {
     "import/first": "error",
     "import/newline-after-import": "error",
     "import/no-duplicates": "error",
-    "linebreak-style": ["error", "windows"],
+    "linebreak-style": ["error", "unix"],
     'prettier/prettier': [
       'error',
       {
